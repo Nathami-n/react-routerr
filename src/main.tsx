b2060b9d6c22fb5c6011ce8e9f050c9149f63277
Nathami-n <georgenathan010@gmail.com>
2024-06-04 22:17:17 +0300

    did nesting

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import Root from './routes/root';
 import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+import ErrorPage from './error-page';
+import Contact from './routes/contact';
+
+const router = createBrowserRouter([
+ {
+  path: "/",
+  element: <Root/>,
+  errorElement: <ErrorPage/>,
+  children: [
+    {
+      path: "contacts/:contactId",
+      element: <Contact/>
+     }
+  ]
+ }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
